5fe2e345468512c13de739b039d3faa2216c08d0
Hristo Stoyanov <hr.stoyanov@gmail.com>
2018-11-30 18:05:11 +0200

    Upgrade to UIKit 3.0.0.-rc.25

--- gwt-uikit/src/main/java/com/peruncs/gwt/uikit/UIKit.java
+++ gwt-uikit/src/main/java/com/peruncs/gwt/uikit/UIKit.java
@@ -11,9 +11,13 @@ public abstract class UIKit {
     //Does not work yet, see: https://github.com/uikit/uikit/issues/3676
     //Notifications API
     public static native UKNotification notification(String message);
+
     public static native UKNotification notification(String message, String status);
+
     public static native UKNotification notification(String message, UKNotificationOptions options);
+
     public static native UKNotification notification(UKNotificationOptions options);
+
     public static native UKNotification notification(Node node, UKNotificationOptions options);
 
     @JsMethod(name = "closeAll", namespace = "UIkit.notification")
